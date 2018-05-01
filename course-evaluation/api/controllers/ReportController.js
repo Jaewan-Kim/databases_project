562a7b50057ff24e197e025ed8f8ed3e78cd7128
Jay <jay@172-16-25-167.DYNAPOOL.NYU.EDU>
2018-04-30 20:02:21 -0400

    CHART only

--- course-evaluation/api/controllers/ReportController.js
+++ course-evaluation/api/controllers/ReportController.js
@@ -62,21 +62,33 @@ module.exports = {
 		var courseid = req.param('courseid','')
 		var email = req.session.userEmail
 		var week = req.param('week','')
+		var blank = ''
 		if(req.param('reporttype','')== "comments"){
 
 			
 			if(week == 0){
-				StudentReport.query('SELECT r.comments FROM student_report sr, reports r WHERE sr.course_id = ? AND sr.report_id = r.report_id',[courseid],function(err,comments){
+				StudentReport.query('SELECT r.comments FROM student_report sr, reports r WHERE sr.course_id = ? AND sr.report_id = r.report_id AND r.comments<> ?',[courseid, blank],function(err,comments){
 					comments= JSON.parse(JSON.stringify(comments))
+					if(comments.length==0){
+
+						req.flash('error', 'There are no comments available for this course/week')
+						res.view('pages/adminpage', {courses:req.session.courses})
+						return;
+					}
 
 					res.view('pages/commentspage',{comments:comments})
 
 				})
 			}
 			else{
-				StudentReport.query('SELECT r.comments FROM student_report sr, reports r WHERE sr.course_id = ? AND sr.report_id = r.report_id AND sr.week= ?',[courseid, week],function(err,comments){
+				StudentReport.query('SELECT r.comments FROM student_report sr, reports r WHERE sr.course_id = ? AND sr.report_id = r.report_id AND sr.week= ? AND r.comments<> ?',[courseid, week, blank],function(err,comments){
 					comments= JSON.parse(JSON.stringify(comments))
+					if(comments.length==0){
 
+						req.flash('error', 'There are no comments available for this course/week')
+						res.view('pages/adminpage', {courses:req.session.courses})
+						return;
+					}
 					res.view('pages/commentspage',{comments:comments})
 
 
@@ -84,8 +96,134 @@ module.exports = {
 			}
 		}
 		else{
+			if(week==0){
+				StudentReport.query('SELECT r.* FROM student_report sr,reports r WHERE sr.course_id = ? AND r.report_id = sr.report_id',[courseid],function(err, reports){
+					reports= JSON.parse(JSON.stringify(reports))
+					if (reports.length == 0){
+						req.flash('error', 'There are no reports available for this course/week')
+						res.view('pages/adminpage', {courses:req.session.courses})
+
+					}
+					console.log(reports)
+					let reportslist = new Promise(function(resolve, reject){
+						var r1=0
+						var r2=0
+						var r3=0
+						var r4=0
+						var r5=0
+						var x=0
+						for (i = 0; i < reports.length;i++){
+							r1 = r1+reports[i].response_1
+							r2 = r2+reports[i].response_2
+							r3 = r3+reports[i].response_3
+							r4 = r4+reports[i].response_4
+							r5 = r5+reports[i].response_5
+							console.log(reports[i].response_1)
+							console.log(r1)
+
+							x++;
+							if(reports.length == x){
+								
+								var total =reports.length
+								var raw={
+									response1:r1,									
+									response2:r2,									
+									response3:r3,									
+									response4:r4,									
+									response5:r5,									
+								}
+								var ratio = {
+									reponse1:r1/total,
+									reponse2:r2/total,
+									reponse3:r3/total,
+									reponse4:r4/total,
+									reponse5:r5/total
+								}
+								var response={
+									raw:raw,
+									ratio:ratio,
+									total:reports.length
+								}
+								
+								resolve(response)
+							}
+						}
+
+					})
+
+					reportslist.then(function(response){
+						console.log(response)
+						res.view('pages/oneclass')
+					})
+
+
+				})
+			}
+			else{
+				StudentReport.query('SELECT r.* FROM student_report sr, reports r WHERE sr.week = ? AND sr.course_id = ? AND r.report_id = sr.report_id',[week, courseid],function(err, reports){
+					reports= JSON.parse(JSON.stringify(reports))
+					if (reports.length == 0){
+						req.flash('error', 'There are no reports available for this course/week')
+						res.view('pages/adminpage', {courses:req.session.courses})
+					}
+
+					let reportslist = new Promise(function(resolve, reject){
+						var r1=0
+						var r2=0
+						var r3=0
+						var r4=0
+						var r5=0
+						var x=0
+						for (i = 0; i < reports.length;i++){
+							r1 = r1+reports[i].response_1
+							r2 = r2+reports[i].response_2
+							r3 = r3+reports[i].response_3
+							r4 = r4+reports[i].response_4
+							r5 = r5+reports[i].response_5
+							console.log(reports[i].response_1)
+							console.log(r1)
+
+							x++;
+							if(reports.length == x){
+								
+								var total =reports.length
+								var raw={
+									response1:r1,									
+									response2:r2,									
+									response3:r3,									
+									response4:r4,									
+									response5:r5,									
+								}
+								var ratio = {
+									reponse1:r1/total,
+									reponse2:r2/total,
+									reponse3:r3/total,
+									reponse4:r4/total,
+									reponse5:r5/total
+								}
+								var response={
+									raw:raw,
+									ratio:ratio,
+									total:reports.length
+								}
+								resolve(response)
+							}
+						}
+
+					})
+
+					reportslist.then(function(response){
+						console.log(response)
+					})
+
+					
+
+				})
+			}
 
 		}
+
+
 	},
 	totwoclasses:function(req,res){
 		res.view('pages/twoclasses',{courses:req.session.courses})
